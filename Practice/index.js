e7112ce0758fbdf93fb84874e07987417c62430d
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-08 20:34:06 +0530

    sixth commit

--- Practice/index.js
+++ Practice/index.js
@@ -3,14 +3,23 @@
 
 //DAY-2
 // console.log(a);
+// function x(){
+//     let a=7;
+//     function y(){
+//         let a=2;
+//         console.log(a);
+//     }
+    
+//     return y;
+// }
+// var z=x();
+// console.log(z);
+// z();
+//7 is printed
 function x(){
     var a=7;
-    function y(){
-        console.log(a);
-    }
-    return y;
+    console.log(a);
 }
-var z=x();
-console.log(z);
-z();
-//7 is printed
+var b=3;
+console.log(b);
+x();
