ec5face6f79d344f159cfd0c36b856f4dd171f0e
MeghashreeK <kundermeghashree279@gmail.com>
2024-07-09 14:55:20 +0530

    eight commit

--- Practice/Day3.js
+++ Practice/Day3.js
@@ -1,12 +1,13 @@
-function x(){
-    for(var i=1;i<=5;i++){
+// function x(){
+//     for(var i=1;i<=5;i++){
 
-        setTimeout(function y(){
-           console.log(i);
-        },i*1000);
-    }
-}
-x();
+//         setTimeout(function y(){
+//            console.log(i);
+//         },i*1000);
+//     }
+// }
+// x();
+//output: here it prints 6, 6 times after each seconds.
 
 // function x(){
 //     for(var i=1;i<=5;i++){
@@ -19,3 +20,15 @@ x();
 //     }
 // }
 // x();
+//output: here it prints 1 to 5, after each seconds.
+
+function a(){
+    let x=2;
+    function b(){
+        console.log(x);
+    }
+    return b;
+}
+let x=6;
+let z=a();
+z();
